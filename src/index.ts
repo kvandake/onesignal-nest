a2ef85a4a2a9db822f93bdf58ec4314f091a8fcd
kvandake <kvandake@gmail.com>
2019-08-29 23:47:10 +0300

    Refactoring

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './onesignal.module';
+export * from './onesignal.constants';
+export * from './onesignal.provider';
+export * from './onesignal.service';
+export * from './interfaces';
--- src/module/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './onesignal.module'
-export * from './onesignal.constants'
-export * from './onesignal.provider'
-export * from './onesignal.service'
-export * from './interfaces'
